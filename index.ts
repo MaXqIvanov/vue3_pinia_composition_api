03309721e31012b79ed9522e2a16947439f58b9e
MaXqIvanov <maksivanov35@ya.ru>
2022-12-16 15:16:16 +0300

    update

--- index.ts
+++ index.ts
@@ -1,12 +1,10 @@
-import { Request, Response } from "express";
-
 const express = require('express');
 const path = require('path');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('/*', (req: Request, res: Response) => {
+app.get('/*', (req, res) => {
     res.sendFile(`${__dirname}/dist/index.html`);
 });
 
